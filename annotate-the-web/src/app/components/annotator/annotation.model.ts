79cdd39829afba5fa95e3cd385f155a5ef325570
Geoff Dryer <geoff@universalsourcellc.com>
2019-10-01 23:28:25 -0500

    Added a annotation component to encapsulate the logic of selecting text to make annotations; added logic to listen for selecting text only on those elements that should be part of the annotation area

---
+++ annotate-the-web/src/app/components/annotator/annotation.model.ts
@@ -0,0 +1,3 @@
+export interface Annotation {
+	selectedText: string;
+}
--- annotate-the-web/src/app/components/annotator/annotator.component.ts
+++ annotate-the-web/src/app/components/annotator/annotator.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { debounceTime, filter, map } from "rxjs/operators";
+import { Annotation } from './annotation.model';
 
 @Component({
   selector: 'app-annotator',
   templateUrl: './annotator.component.html',
   styleUrls: ['./annotator.component.scss']
 })
-export class AnnotatorComponent implements OnInit {
-
+export class AnnotatorComponent implements OnInit, AfterViewInit {
+	enabledClass = 'annotate-enabled';
+	@ViewChild('annotator') annotatorElem: ElementRef;
   constructor() { }
 
   ngOnInit() {
-  }
+	}
+
+	ngAfterViewInit(): void {
+		this.annotatorElem.nativeElement;
+		fromEvent(document, 'selectionchange').pipe(
+			debounceTime(1500), //only listen after the user has selected and paused
+			map(e => document.getSelection()),
+			filter(selection => {
+				const {anchorNode: {parentElement: {className}}} = selection; //get class name of parent
+				return !className ? false : className.includes(this.enabledClass); //only continue if the highlighted text has the annotation enabled class
+			}),
+			map(selection => {
+				return {
+					selectedText: selection.anchorNode.textContent.substring(
+						selection.focusOffset,
+						selection.anchorOffset)
+				} as Annotation;
+			})
+		).subscribe((annotation: Annotation) => {
+				const annot = annotation;
+
+			});
+	}
 
 }
